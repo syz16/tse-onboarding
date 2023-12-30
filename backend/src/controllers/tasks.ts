d4167d8f9911d35cb4054d592549c0ec3c53af9f
Sophia Zhu <sophiazhu16@gmail.com>
2023-12-30 01:19:17 -0800

    removed a console.log from debugging

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -3,7 +3,6 @@ import TaskModel from "src/models/task";
 
 export const getAllTasks: RequestHandler = async (req, res, next) => {
   try {
-    console.log("here 1");
     const tasks = await TaskModel.find({}).sort("-dateCreated");
     res.status(200).json(tasks);
   } catch (error) {
