e7aa2081702d7d543cd92f92071790dbddcc4456
Sophia Zhu <sophiazhu16@gmail.com>
2024-01-03 00:11:16 -0800

    Part 2.2 update component TaskItem with links to task detail

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import type { Task } from "src/api/tasks";
 import { updateTask } from "src/api/tasks";
 import { CheckButton } from "src/components";
@@ -34,7 +35,9 @@ export function TaskItem({ task: initialTask }: TaskItemProps) {
     <div className={styles.item}>
       {<CheckButton checked={task.isChecked} onPress={handleToggleCheck} disabled={isLoading} />}
       <div className={checkedClass}>
-        <span className={styles.title}>{task.title}</span>
+        <Link to={`/task/${task._id}`}>
+          <span className={styles.title}>{task.title}</span>
+        </Link>
         {task.description && <span className={styles.description}>{task.description}</span>}
       </div>
     </div>
